84b1748739c31a3a46826f63642753c2da13c9a3
Santiago-1505 <142053757+Santiago-1505@users.noreply.github.com>
2023-08-23 19:23:52 -0500

    Create Ejercicio_Promedio.java

---
+++ Ejercicio_Promedio.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      // Definir los números
+        int numero_1 = 40;
+        int numero_2 = 50;
+        int numero_3 = 60;
+        int numero_4 = 24;
+        int numero_5 = 66;
+        int numero_6 = 12;
+        int numero_7 = 98;
+
+        // Calcular el promedio
+        double promedio = (numero_1 + numero_2 + numero_3 + numero_4 + numero_5 + numero_6 + numero_7) / 7.0;
+
+        // Mostrar el promedio
+        System.out.println("El promedio de los números es: " + promedio);
+
+        // Comparar cada número con el promedio
+        if (numero_1 > promedio) {
+            System.out.println(numero_1 + " está por encima del promedio.");
+        } else if (numero_1 < promedio) {
+            System.out.println(numero_1 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_1 + " es igual al promedio.");
+        }
+        if (numero_2 > promedio) {
+            System.out.println(numero_2 + " está por encima del promedio.");
+        } else if (numero_2 < promedio) {
+            System.out.println(numero_2 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_2 + " es igual al promedio.");
+        }
+        if (numero_3 > promedio) {
+            System.out.println(numero_3 + " está por encima del promedio.");
+        } else if (numero_3 < promedio) {
+            System.out.println(numero_3 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_3 + " es igual al promedio.");
+        }
+        if (numero_4 > promedio) {
+            System.out.println(numero_4 + " está por encima del promedio.");
+        } else if (numero_4 < promedio) {
+            System.out.println(numero_4 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_4 + " es igual al promedio.");
+        }
+        if (numero_5 > promedio) {
+            System.out.println(numero_5 + " está por encima del promedio.");
+        } else if (numero_5 < promedio) {
+            System.out.println(numero_5 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_5 + " es igual al promedio.");
+        }
+        if (numero_6 > promedio) {
+            System.out.println(numero_6 + " está por encima del promedio.");
+        } else if (numero_6 < promedio) {
+            System.out.println(numero_6 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_6 + " es igual al promedio.");
+        }
+        if (numero_7 > promedio) {
+            System.out.println(numero_7 + " está por encima del promedio.");
+        } else if (numero_7 < promedio) {
+            System.out.println(numero_7 + " está por debajo del promedio.");
+        } else {
+            System.out.println(numero_7 + " es igual al promedio.");
+        }
+
+        
+    }
+}
